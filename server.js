c53205e9c8cc5da479f50386d29c57db12ecb801
tarasdz12367@gmail.com <Lenovoa337>
2025-05-20 11:08:34 +0300

    update

--- server.js
+++ server.js
@@ -5,9 +5,50 @@ const { setupMessageHandler } = require('./handlers/messageHandler');
 const { setupCallbackQueryHandler } = require('./handlers/callbackQueryHandler');
 const data1cRouter = require('./routes/data1C.router');
 const {cron_job} = require('./services/cronjob.service')
+const { rateLimit } = require('express-rate-limit');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 8088;
+
+
+const getClientKey = (req) => {
+    const accessToken = req.cookies?.access_token;
+    return accessToken || req.ip;
+};
+
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    limit: 100,
+    keyGenerator: getClientKey,
+    standardHeaders: 'draft-8',
+    legacyHeaders: false,
+
+    handler: (req, res) => {
+        return res.status(429).json({ message: "Too many requests, please try again later." });
+    }
+})
+
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        console.log({origin});
+        // if (constants.allowedCorsOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        // } else {
+        //     callback(new Error('Not allowed by CORS'));
+        // }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization', 'x-refresh-token'],
+    credentials: true,
+};
+
+app.use(limiter)
+app.use(cors(corsOptions));
+
 
 cron_job.start();
 
