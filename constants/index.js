4d09c34237db2ce3e1b1fcbcfe1cc1c5eb4a00bc
tarasdz12367@gmail.com <Lenovoa337>
2025-08-01 14:38:15 +0300

    update

--- constants/index.js
+++ constants/index.js
@@ -9,7 +9,7 @@ module.exports = {
     data1C_auth: process.env.DATA1C_AUTH || '',
 
     db_port: process.env.DB_PORT || 5000,
-    db_host: process.env.DB_HOST || '127.0.0.1',
+    db_host: process.env.DB_HOST || '',
     db_name: process.env.DB_NAME || '',
     db_user: process.env.DB_USER || '',
     db_password: process.env.DB_PASS || '',
--- database/index.js
+++ database/index.js
@@ -1,14 +1,12 @@
 const { Sequelize } = require('sequelize');
 const constants = require('../constants');
 
-console.log(constants.db_host)
-console.log(constants.db_name)
 
 const sequelize = new Sequelize(constants.db_name, constants.db_user, constants.db_password, {
     host: constants.db_host,
     port: constants.db_port,
     dialect: constants.db_dialect,
-    logging: false,
+    logging: true,
     // define: {
     //     schema: 'square'
     // },
