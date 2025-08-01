69d0df3119a74fca588cec70c0e4ad684ed2ec2f
tarasdz12367@gmail.com <Lenovoa337>
2025-08-01 14:49:19 +0300

    update

--- database/index.js
+++ database/index.js
@@ -1,18 +1,11 @@
 const { Sequelize } = require('sequelize');
 const constants = require('../constants');
 
-console.log(constants.db_host)
-console.log(constants.db_port)
-console.log(constants.db_dialect)
-console.log(constants.db_name)
-console.log(constants.db_user)
-console.log(constants.db_password)
-
 const sequelize = new Sequelize(constants.db_name, constants.db_user, constants.db_password, {
     host: constants.db_host,
     port: constants.db_port,
     dialect: constants.db_dialect,
-    logging: true,
+    logging: false,
     // define: {
     //     schema: 'square'
     // },
